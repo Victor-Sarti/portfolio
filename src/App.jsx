e7a6e5327e9e55cf4a1b9b0a525c31525efb2b02
Victor-sarti <Sartivictor24@gmail.com>
2025-01-27 22:34:08 -0300

    feat: hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 import Header from './components/Header'
+import Hero from './components/Hero'
 const App = () => {
   return (
+    <>
     <Header/>
+    <main>
+      <Hero/>
+    </main>
+    </>
   )
 }
 
