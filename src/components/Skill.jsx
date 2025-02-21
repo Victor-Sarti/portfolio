bc1ea79d68d0953c2f3509c86f599eee8265af8c
Victor-sarti <Sartivictor24@gmail.com>
2025-02-21 14:24:12 -0300

    feat: Simg

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,46 +1,23 @@
-const skillItem = [
-    {
-      imgSrc: '/images/figma.svg',
-      label: 'Figma',
-      desc: 'Design tool'
-    },
-    {
-      imgSrc: '/images/css3.svg',
-      label: 'CSS',
-      desc: 'User Interface'
-    },
-    {
-      imgSrc: '/images/javascript.svg',
-      label: 'JavaScript',
-      desc: 'Interaction'
-    },
-    {
-      imgSrc: '/images/nodejs.svg',
-      label: 'NodeJS',
-      desc: 'Web Server'
-    },
-    {
-      imgSrc: '/images/expressjs.svg',
-      label: 'ExpressJS',
-      desc: 'Node Framework'
-    },
-    {
-      imgSrc: '/images/mongodb.svg',
-      label: 'MongoDB',
-      desc: 'Database'
-    },
-    {
-      imgSrc: '/images/react.svg',
-      label: 'React',
-      desc: 'Framework'
-    },
-    {
-      imgSrc: '/images/tailwindcss.svg',
-      label: 'TailwindCSS',
-      desc: 'User Interface'
-    },
-  ];
+import SkillCard from "./SkillCard";
+import figma from "../assets/figma.svg"
+import css from "../assets/css3.svg"
+import js from "../assets/javascript.svg"
+import nodejs from "../assets/nodejs.svg"
+import expo from "../assets/expo.png"
+import sql from "../assets/mysql.png"
+import react from "../assets/react.svg"
+import tailwind from "../assets/tailwindcss.svg"
 
+const skillItem = [
+  { imgSrc: figma, label: 'Figma', desc: 'Design tool' },
+  { imgSrc: css, label: 'CSS', desc: 'User Interface' },
+  { imgSrc: js, label: 'JavaScript', desc: 'Interaction' },
+  { imgSrc: nodejs, label: 'NodeJS', desc: 'Web Server' },
+  { imgSrc: expo, label: 'Expo', desc: 'Framework for React Native' },
+  { imgSrc: sql, label: 'MySql', desc: 'Database' },
+  { imgSrc: react, label: 'React', desc: 'Framework' },
+  { imgSrc: tailwind, label: 'TailwindCSS', desc: 'User Interface' },
+];
 
 const Skill = () => {
   return (
@@ -52,16 +29,21 @@ const Skill = () => {
 
             <p className=" text-zinc-400 mt-3 mb-8 max-w-[50ch]">
             Discover the powerful tools and technologies
-            I use to create exceptional, high-performing 
+      I use to create exceptional, high-performing 
             websites & applications.
             </p>
 
-            <div className="">
+            <div className=" grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr'))]">
                 {
                     skillItem.map(({imgSrc, label, desc}, key) => 
                     (
                     
-                     'SkillCard'
+                     <SkillCard 
+                     key={key}
+                     imgSrc={imgSrc}
+                     label={label}
+                     desc={desc}
+                     />
                     ))
                 }
             </div>
