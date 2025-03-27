57dd46669fee0742ab3b371d5f5c1e91403740d9
Victor-sarti <Sartivictor24@gmail.com>
2025-03-26 22:01:02 -0300

    feat: import

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import { ReactLenis } from 'lenis/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
 import React from 'react'
 
 import Header from './components/Header';
@@ -7,19 +15,40 @@ import Skill from './components/Skill';
 import Work from './components/Work';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+
+
 const App = () => {
+
+  useGSAP(() => {
+    const elements = gsap.utils.toArray('.reveal-up');
+
+    elements.forEach((element) => {
+      gsap.to(element, {
+        ScrollTrigger: {
+          trigger: element,
+          scrub: true,
+          markers: true
+        },
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: 'power2.out'
+      })
+    })
+  })
+
   return (
-    <>
-    <Header/>
-    <main>
-      <Hero/>
-      <About/>
-      <Skill/>
-      <Work/>
-      <Contact/>
-      <Footer/>
-    </main>
-    </>
+    <ReactLenis root>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Skill />
+        <Work />
+        <Contact />
+      </main>
+      <Footer />
+    </ReactLenis>
   )
 }
 
