d292f76e1b40057aac67602735dc76afc84c0e96
Victor-sarti <Sartivictor24@gmail.com>
2025-03-26 22:01:50 -0300

    feat: reveal-up

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
     return (
         <section id="about" className='section' >
             <div className="container">
-                <div className=' bg-zinc-800/50 p-7 rounded-2xl md:p-12'>
+                <div className=' bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up'>
                     <p className=" text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi,
                         illo exercitationem cum eos voluptas quis quam commodi. Dignissimos
